2d16eed6533e3a0c27cf95d9b4f9ed72a6bb4cd7
Krishna Chaitanya Chinnam <s2450029@uhtred.inf.ed.ac.uk>
2023-06-02 15:23:18 +0100

    Change script to create data sets to remove extra classes

--- myapps/SpeechT5/SpeechT5/create_sa_slue_data.sh
+++ myapps/SpeechT5/SpeechT5/create_sa_slue_data.sh
@@ -1,13 +1,17 @@
 
-PATH_VOXCELEB=
+PATH_VOXCELEB=/home/s2450029/repos/slue-toolkit/data/slue-voxceleb
+PATH_DESTINATION=/home/s2450029/repos/t2s-xling/data_formatted/fairseq/speech/tsv/sa_slue
 
-echo "/disk/scratch1/ramons/data/t2s-xling/data/speech/slue-voxceleb/fine-tune_raw" > /disk/scratch1/ramons/data/t2s-xling/data_formated/fairseq/speech/tsv/sa_slue/train.tsv
-cat /disk/scratch1/ramons/data/t2s-xling/data/speech/slue-voxceleb/slue-voxceleb_fine-tune.tsv | awk '{print $1".flac\t"($NF-$(NF-1))*16000"\t"$(NF-2)}' | tail  -n +2 >> /disk/scratch1/ramons/data/t2s-xling/data_formated/fairseq/speech/tsv/sa_slue/train.tsv
-
-
-echo "/disk/scratch1/ramons/data/t2s-xling/data/speech/slue-voxceleb/dev_raw" > /disk/scratch1/ramons/data/t2s-xling/data_formated/fairseq/speech/tsv/sa_slue/valid.tsv
-cat /disk/scratch1/ramons/data/t2s-xling/data/speech/slue-voxceleb/slue-voxceleb_dev.tsv | awk '{print $1".flac\t"($NF-$(NF-1))*16000"\t"$(NF-2)}' |  tail  -n +2 >> /disk/scratch1/ramons/data/t2s-xling/data_formated/fairseq/speech/tsv/sa_slue/valid.tsv
+#echo $PATH_VOXCELEB/fine-tune_raw
+#echo "$PATH_DESTINATION/train.tsv"
 
 
-cat /disk/scratch1/ramons/data/t2s-xling/data/speech/slue-voxceleb/slue-voxceleb_dev.tsv /disk/scratch1/ramons/data/t2s-xling/data/speech/slue-voxceleb/slue-voxceleb_fine-tune.tsv | awk '{print $(NF-2)}' | sort | uniq | awk '{print $1" "1}' > /disk/scratch1/ramons/data/t2s-xling/data_formated/fairseq/speech/tsv/sa_slue/dict.txt
+
+echo "$PATH_VOXCELEB/fine-tune_raw" > $PATH_DESTINATION/train.tsv
+cat $PATH_VOXCELEB/slue-voxceleb_fine-tune.tsv | awk -v FS='\t' '{if($5!="<mixed>")print $1".flac\t"($NF-$(NF-1))*16000"\t"$(NF-2)}' | tail  -n +2 >> $PATH_DESTINATION/train.tsv
+
+echo "$PATH_VOXCELEB/dev_raw" > $PATH_DESTINATION/valid.tsv
+cat $PATH_VOXCELEB/slue-voxceleb_dev.tsv | awk -v FS='\t' '{if($5!="<mixed>" && $5!="Disagreement")print $1".flac\t"($NF-$(NF-1))*16000"\t"$(NF-2)}' |  tail  -n +2 >> $PATH_DESTINATION/valid.tsv
+
+cat $PATH_VOXCELEB/slue-voxceleb_dev.tsv $PATH_VOXCELEB/slue-voxceleb_fine-tune.tsv | awk -v FS='\t' '{if($5!="<mixed>" && $5!="Disagreement" && $5!="sentiment")print $(NF-2)}' | sort | uniq | awk '{print $1" "1}' > $PATH_DESTINATION/dict.txt
 
