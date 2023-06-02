174e537b6bcbadb5235466e61e16d54369fb5213
Krishna Chaitanya Chinnam <s2450029@uhtred.inf.ed.ac.uk>
2023-06-02 15:24:35 +0100

    Change max-speech-positions to 250000

--- myapps/SpeechT5/SpeechT5/decode_sa_slue.sh
+++ myapps/SpeechT5/SpeechT5/decode_sa_slue.sh
@@ -1,14 +1,14 @@
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 . ./activate.sh
 
 
-CHECKPOINT_PATH=/disk/scratch2/ramons/data/t2s-xling/models/speechT5/fairseq/speecht5_base/slue_sa_fixed/checkpoint_best.pt
-DATA_ROOT=/disk/scratch1/ramons/data/t2s-xling/data_formated/fairseq/speech/tsv/sa_slue/
+CHECKPOINT_PATH=/home/s2450029/repos/t2s-xling/models/speechT5/fairseq/speecht5_base/slue_sa/checkpoint_best.pt
+DATA_ROOT=/home/s2450029/repos/t2s-xling/data_formatted/fairseq/speech/tsv/sa_slue/
 SUBSET=valid
 USER_DIR=speecht5
-RESULTS_PATH=/disk/scratch1/ramons/data/t2s-xling/models/speechT5/fairseq/slue_sa/result
+RESULTS_PATH=/home/s2450029/repos/t2s-xling/models/speechT5/fairseq/slue_sa/result
 
 mkdir -p ${RESULTS_PATH}
 
@@ -21,5 +21,5 @@ python scripts/generate_class.py ${DATA_ROOT} \
   --path ${CHECKPOINT_PATH} \
   --results-path ${RESULTS_PATH} \
   --batch-size 1 \
-  --max-speech-positions 8000 \
+  --max-speech-positions 250000 \
   --sample-rate 16000
